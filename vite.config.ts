519676716196cd68577f696824874c3deb897dd6
sarowarhosen1101-code <sarowarhosen1101@gmail.com>
2026-06-06 12:19:59 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/finance/'
+    base: '/finance/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
